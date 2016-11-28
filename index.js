82a47466ca8dd7d958b523e28e62f80132ac8d45
Arnaud Benard <arnaudm.benard@gmail.com>
2016-11-28 13:43:00 +0000

    to es5

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-module.exports = (list) => {
+module.exports = function (list) {
   // build histogram
-  var hist = Array.prototype.reduce.call(list, (acc, value) => {
+  var hist = Array.prototype.reduce.call(list, function (acc, value) {
     acc[value] = (acc[value] || 0) + 1;
     return acc;
   }, {});
 
   // Sort values and return keys
-  return Object.keys(hist).sort((a, b) => hist[b] - hist[a])
+  return Object.keys(hist).sort(function (a, b) { return hist[b] - hist[a]; })
   // Remap the sorted key to an object
   .map(key => {
     var obj = {};
